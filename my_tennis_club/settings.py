d2fc42b8c5b6f6d5f782e16e27c08df106b0e886
omarzinane1 <zinaneomar2000@gmail.com>
2026-09-01 15:40:23 +0100

    create aws account create database in rds and connect django app to database

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "omarhiba",
+        "HOST": "database-1.cfs0qyuwuuof.eu-north-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
